0979d684832427e43971a0eb6f8ac679f64f69ba
deasble <hdjs0814@naver.com>
2022-05-31 14:26:01 +0900

    header title 변경
    header title 변경

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,25 @@
 <template>
   <div class="Header">
-    <h1> 투두 리스트</h1>
-    <p>전체 할일: {{ ListLength }} / 완료된 할일: {{ ListDone }} / 남은 할일: {{ ListLength-ListDone }}</p>
+    <h1>Todo List</h1>
+    <p>
+      전체 할일: {{ ListLength }} / 완료된 할일: {{ ListDone }} / 남은 할일:
+      {{ ListLength - ListDone }}
+    </p>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 
 export default {
   computed: {
-    ...mapGetters(['ListLength', 'ListDone'])
+    ...mapGetters(["ListLength", "ListDone"]),
   },
-}
+};
 </script>
 
 <style  scoped>
-.Header h1{
-    padding: 0 0 10px;
+.Header h1 {
+  padding: 0 0 10px;
 }
 </style>
